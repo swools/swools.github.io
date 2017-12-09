4e87957955eb00d43f41213417cfa7f40ee24365
Steve Wool <stevewool7@gmail.com>
2017-12-09 11:38:57 -0500

    Class 7 changes

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,32 @@
+var product = 10 * 10;
+
+var quotient = 100/10;
+
+var difference = 50-17;
+
+var sum = 50+17;
+
+var name = "Steve " + "Wool";
+
+console.log(product + quotient + difference, sum, name);
+
+function onra() {
+    var city = 'Saigon';
+    console.log(`Last Tango In ${city}`);
+}
+
+onra();
+onra();
+onra();
+
+var hello = document.getElementById('hello');
+
+hello.style.color = 'red';
+
+
+
+
+
+
+
+

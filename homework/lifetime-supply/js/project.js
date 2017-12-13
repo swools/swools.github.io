e3325ca66bea5b5fde1c4aca5af3cfc0f2053a66
Steve Wool <stevewool7@gmail.com>
2017-12-13 18:20:53 -0500

    Class 8 Edits

---
+++ homework/lifetime-supply/js/project.js
@@ -0,0 +1,11 @@
+// Store your current age into a variable
+
+// Store a maximum age into a variable
+
+// Store a favorite drink (from a drop-down) into a variable
+
+// Store an amount per day into a variable
+
+// Calculate how much you would drink for the rest of your life!
+
+// Output your results to the user

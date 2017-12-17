aea64ce3ea8a4e01da1167bcfbb1478c2059bc58
Steve Wool <stevewool7@gmail.com>
2017-12-17 14:59:01 -0500

    Homework lifetime-supply

--- homework/lifetime-supply/js/project.js
+++ homework/lifetime-supply/js/project.js
@@ -9,3 +9,22 @@
 // Calculate how much you would drink for the rest of your life!
 
 // Output your results to the user
+
+$('#click-me').click(function(){
+    var age = $('#age').val();
+    
+    var maxAge = $('#max-age').val();
+    
+    var drink = $('#item').val();
+    
+    var amount = $('#num-per-day').val();
+    
+
+    
+    var solution = (((maxAge-age)*365)* amount);
+    $('#solution').html(solution);
+    $('#drink').html(drink);
+    console.log(solution);
+
+})
+

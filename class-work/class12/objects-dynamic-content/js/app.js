476264c3b9cc51f629aa8649f85594432b042dbe
Steve Wool <stevewool7@gmail.com>
2018-01-03 18:26:48 -0500

    Downloaded Class 12 Classwork

---
+++ class-work/class12/objects-dynamic-content/js/app.js
@@ -0,0 +1,16 @@
+$('document').ready(function(){
+
+  // Set the body's background color to equal the pageLoadData object's backgroundColor property
+
+  // In .elem-1 set the background image, and the content inside .elem-1__title and .elem-1_date
+
+  // In .elem-2 set the background image, and the content inside .elem-2__title and .elem-2_date
+
+  // In .elem-3 set the background image, and the content inside .elem-3__title and .elem-3_date
+
+  // In .elem-4 set the background image, and the content inside .elem-4__title and .elem-4_date
+
+  // Put the cteated at date from our data object into the footer's h1
+
+})
+

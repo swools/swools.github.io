f07af0596c8462d1128f5bb2732803f02a95d761
Steve Wool <stevewool7@gmail.com>
2018-01-10 19:18:44 -0500

    Class 13 updates

---
+++ class-work/class13/list-maker/js/app.js
@@ -0,0 +1,53 @@
+// Users should be able to:
+// 1. Enter an item into #item
+// 2. Click on #clickme
+
+
+// 3. New item is appended as a <li> element to the #list
+// 4. After item is added, the text inside #item should clear
+
+// Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called when #clickme is clicked and appends the <li> to #list
+
+// Bonus: Focus on #item after the item is added (hint: look up "jquery focus")
+// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (hint: use an if/else statement)
+// Legendary Bonus: Remove individual <li> elements when they are clicked
+
+$(document).ready(function () {
+
+	
+
+	
+	$('#clickme').click(function(){
+		var newItem = $('#item').val();
+		
+		if (newItem == ""){
+			alert("You have not added anything to your list");
+		} else {
+			appendItem(newItem);
+
+		}
+	})
+
+
+
+
+	// Appends input value as new line item
+	function appendItem(item){
+		$('#list').append("<li>" + item + '</li>');
+		$('#item').val("");
+		$('#item').focus();
+	}
+		 
+
+	// Removes li when clicked
+	$('ul').on("click", "li", function(){
+		$(this).remove();
+	})
+
+	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
+	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
+	// 1B. This anonymous function should call a separate function, appendItem, and pass it the variable newItem
+	// 1C. This anonymous function should then clear the text inside of #item
+
+	// 2. Write the appendItem function here
+})

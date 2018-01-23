c97e435f68f1471e23b9dcc11c9e068edf9af0b8
Steve Wool <stevewool7@gmail.com>
2018-01-22 21:22:57 -0500

    Class 16 updates

--- class-work/class16/animated-sticky-nav/js/app.js
+++ class-work/class16/animated-sticky-nav/js/app.js
@@ -1,2 +1,10 @@
 $(document).ready(function () {
+    $(window).scroll(function(){
+        
+        if ($(window).scrollTop() >  75) {
+            $('header').addClass('small-nav');
+        } else {
+            $('header').removeClass('small-nav');
+        }
+    })
 })
--- class-work/class16/responsive-navbar/js/app.js
+++ class-work/class16/responsive-navbar/js/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
-	// console.log($(window).width());
 
+	$(window).resize(function(){
+		console.log($(window).width());
+
+		if ($(window).width() > '875') {
+			$('#mobile-nav').hide();
+		} 
+	});
+
+
+	
+	$("#burger").click(function(){
+		$("#mobile-nav").slideToggle();
+	})
+
+	
 })
+
+

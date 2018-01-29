fa313227e8c503dd51f8b8e8a3a879c732409248
Steve Wool <stevewool7@gmail.com>
2018-01-29 18:17:07 -0500

    Class 17 updates

---
+++ class-work/class17/masonry-example/js/app.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+
+    $('.grid').masonry({gutter: 10, transitionDuration: '8.8s'});
+})
---
+++ class-work/class17/raindrops-example/js/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    jQuery('#example1').raindrops();  
+});

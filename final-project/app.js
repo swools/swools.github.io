42b286ab48314d4d04b8dfc6b11459e5324e8f11
Steve Wool <stevewool7@gmail.com>
2018-02-04 15:56:18 -0500

    Window resize to create mobile nav

--- final-project/app.js
+++ final-project/app.js
@@ -4,4 +4,41 @@ $(document).ready(function(){
         console.log("hovered");
     })
 
+    
+// memory game
+
+$(function(){
+    $("#memory-game").memoryGame();
+});
+
+//Hamburger menu
+
+$(".mobile-nav").hide();
+
+
+
+
+
+
+
+  
+    
+
+$(window).resize(function(){
+    var pageWidth = $(window).width();
+    if (pageWidth > '900') {
+        $('.fa-bars').hide();
+        $('.mobile-nav').hide();
+        $('.site-nav').show();
+    } else if (pageWidth < '900') {
+        $('.site-nav').hide();
+        $('.fa-bars').show();
+    }
+});
+
+$('.fa-bars').click(function(){
+    $('.mobile-nav').slideToggle();
+});
+    
+    
 })
